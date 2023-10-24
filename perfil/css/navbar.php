1041283a35ac111ff8acd9a770c2528c0b4f211f
carlos-arnoldo-garcia <147547921+carlos-arnoldo-garcia@users.noreply.github.com>
2023-10-24 08:30:45 -0600

    Create navbar.php

---
+++ perfil/css/navbar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CotiEx</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/style.css">
+
+    <!--box icons link-->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <!--google fonts link-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <!--Header-->
+
+    <header>
+        <a href="#" class="logo"><i class='bx bxs-diamond'></i>CotiEx</a>
+
+        <ul class="navlist">
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Equipment</a></li>
+            <li><a href="#">Store</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+
+        <div class="h-main">
+            <a href="#" class="h-btn">Buy Now</a>
+            <div class="bx bx-menu" id="menu-icon"></div>
+            <div class="bx bx-moon" id="darkmode"></div>
+        </div>
+    </header>
