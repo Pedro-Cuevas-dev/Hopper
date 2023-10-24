2943b56b3474cb1ba58b2539750719852956cf8b
carlos-arnoldo-garcia <147547921+carlos-arnoldo-garcia@users.noreply.github.com>
2023-10-24 09:25:51 -0600

    Cambiado variable TELEFONO a Char

--- Cotizaciones BD.sql
+++ Cotizaciones BD.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versión del servidor:         8.0.17 - MySQL Community Server - GPL
+-- Versión del servidor:         8.0.30 - MySQL Community Server - GPL
 -- SO del servidor:              Win64
 -- HeidiSQL Versión:             12.1.0.6537
 -- --------------------------------------------------------
@@ -21,14 +21,14 @@ USE `cotizaciones`;
 
 -- Volcando estructura para tabla cotizaciones.cliente
 CREATE TABLE IF NOT EXISTS `cliente` (
-  `id_client` int(9) NOT NULL,
+  `id_client` int NOT NULL,
   `Nombre` varchar(50) NOT NULL,
   `Apellido` varchar(50) NOT NULL,
   `Correo` varchar(50) NOT NULL,
-  `Telefono` int(11) DEFAULT '0',
+  `Telefono` char(20) DEFAULT NULL,
   `Direccion` varchar(50) NOT NULL DEFAULT '0',
   `Num de Tarjeta` varchar(50) NOT NULL DEFAULT '0',
-  `CVV` int(3) NOT NULL DEFAULT '0',
+  `CVV` int NOT NULL DEFAULT '0',
   `Expiracion` varchar(5) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_client`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -37,17 +37,20 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 
 -- Volcando estructura para tabla cotizaciones.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id_usuarios` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuarios` int NOT NULL AUTO_INCREMENT,
   `user` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `correo` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_usuarios`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Volcando datos para la tabla cotizaciones.usuarios: ~0 rows (aproximadamente)
+INSERT IGNORE INTO `usuarios` (`id_usuarios`, `user`, `password`, `correo`) VALUES
+	(1, 'User1', 'Passowrd', NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+
