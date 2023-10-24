d0ece3a136435061410459e3cc25a768535878e7
carlos-arnoldo-garcia <147547921+carlos-arnoldo-garcia@users.noreply.github.com>
2023-10-24 08:26:57 -0600

    Creado index.php

---
+++ perfil/css/index.php
@@ -0,0 +1,49 @@
+<?php include '../vistas/navbar.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="css/estilos_perfil.css" type="text/css" media="screen">
+            <link rel="" href="">
+            <title>Item Catalog</title>
+        </head>
+        <body id="stylebody">
+            <div class="leftside">
+                <div class="leftsidetop">
+                <h3>Categorias</h2>
+                </div>
+                <hr>
+                <br>
+                <ol><a href="index.php">Datos Personales</a></ol>
+                <br>
+                <ol><a href="Pago.php">Formas De Pago</a></ol>
+                <br>
+                <ol><a href="Compras.php">Compras Anteriores</a></ol>
+                <br>
+                <div class="leftsidebottom">
+                    <h2>Log Out</h2>
+                </div>
+            </div>
+            <div class="rightside">
+                <h1>Tu Perfil</h1>
+                <hr>
+                <h2>Datos Personales</h2><button class="edit">Editar</button>
+                <h3>Nombre:</h3>
+                <p class="highlight">[Relleno] [Relleno] [Relleno]</p> <button class="edit">Editar</button>
+                <h3>Correo:</h3>
+                <p class="highlight">[Relleno] [Relleno] [Relleno]</p> <button class="edit">Editar</button>
+                <h3>Telefono</h3>
+                <p class="highlight">618-555-5523</p>
+
+            </div>
+
+
+
+
+        </body>
+
+</html>
