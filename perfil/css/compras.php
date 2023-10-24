8f5ccc0d79c0ca346f6ff2aafc1800163d077ef2
carlos-arnoldo-garcia <147547921+carlos-arnoldo-garcia@users.noreply.github.com>
2023-10-24 08:30:16 -0600

    Creado compras php

---
+++ perfil/css/compras.php
@@ -0,0 +1,45 @@
+<?php include '../vistas/navbar.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="css/estilos_perfil.css" type="text/css" media="screen">
+            <link rel="" href="">
+            <title>Item Catalog</title>
+        </head>
+        <body id="stylebody">
+        <div class="leftside">
+                <div class="leftsidetop">
+                <h3>Categorias</h2>
+                </div>
+                <hr>
+                <br>
+                <ol><a href="index.php">Datos Personales</a></ol>
+                <br>
+                <ol><a href="Pago.php">Formas De Pago</a></ol>
+                <br>
+                <ol><a href="Compras.php">Compras Anteriores</a></ol>
+                <br>
+                <div class="leftsidebottom">
+                    <h2>Log Out</h2>
+                </div>
+            </div>
+            <div class="rightside">
+                <h1>Tu Perfil</h1>
+                <hr>
+                <h2>Compras Anteriores</h2>
+                <label for="Compra"><strong>Compra: </strong></label><span class="highlight">Nvidia RTX 3060</span>
+
+
+            </div>
+
+
+
+
+        </body>
+
+</html>
