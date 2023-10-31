941662b34c50ac97f28313b753a57e2f42e680fd
carlos-arnoldo-garcia <147547921+carlos-arnoldo-garcia@users.noreply.github.com>
2023-10-31 07:56:39 -0600

    Actualizado a ultima version

--- perfil/php/Datos.php
+++ perfil/php/Datos.php
@@ -1,4 +1,10 @@
-<?php include '../vistas/navbar.php' ?>
+<?php 
+
+session_start();
+include '../vistas/navbar.php';
+include 'guardian.php';
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,11 +13,10 @@
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="css/estilos_perfil.css" type="text/css" media="screen">
-            <link rel="" href="">
+            <link rel="stylesheet" type="text/css" href="../css/style.css">
             <title>Item Catalog</title>
         </head>
-        <body id="stylebody">
+        <body>
             <div class="leftside">
                 <div class="leftsidetop">
                 <h3>Categorias</h2>
@@ -25,24 +30,29 @@
                 <ol><a href="Compras.php">Compras Anteriores</a></ol>
                 <br>
                 <div class="leftsidebottom">
-                    <h2>Log Out</h2>
+                    <h2><a href="Logout.php">Logout</a>
                 </div>
             </div>
             <div class="rightside">
                 <h1>Tu Perfil</h1>
                 <hr>
-                <h2>Datos Personales</h2><button class="edit">Editar</button>
+                <h2>Datos Personales</h2>
                 <h3>Nombre:</h3>
-                <p class="highlight">[Relleno] [Relleno] [Relleno]</p> <button class="edit">Editar</button>
+                <p class="highlight">[Relleno] [Relleno] [Relleno]</p>
+                <br>
+                <button href="#" class="h-btn">Editar</button>
+
                 <h3>Correo:</h3>
-                <p class="highlight">[Relleno] [Relleno] [Relleno]</p> <button class="edit">Editar</button>
+                <p class="highlight">[Relleno] [Relleno] [Relleno]</p>
+                <br>
+                <button href="#" class="h-btn">Editar</button>
+                
                 <h3>Telefono</h3>
                 <p class="highlight">618-555-5523</p>
-
+                <br>
+                <button href="#" class="h-btn">Editar</button>
+                
             </div>
-
-
-
 
         </body>
 
