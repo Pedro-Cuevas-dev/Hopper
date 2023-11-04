284de2f911c2525d41f60b2729ecd572afafe687
Pedro-Cuevas-dev <118694793+Pedro-Cuevas-dev@users.noreply.github.com>
2023-11-04 14:56:14 -0600

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,14 +1,1 @@
-<?php
 
-session_start();
-
-//verificar si el usuario ha iniciado sesion
-if(!isset($_SESSION['username'])){
-    header("location: ../login.php");
-    exit();
-} 
-//mostrar el mensaje de bienvenida si el usuario ha
-echo "bienvenido, " . $_SESSION['username'] . "!<br>"
-
-
-?>
