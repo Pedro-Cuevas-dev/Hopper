ee547ed97d4dc936f3c1293361d73648eff0b4ad
Pedro-Cuevas-dev <118694793+Pedro-Cuevas-dev@users.noreply.github.com>
2023-12-05 10:55:46 -0600

    Create registrar_productos.php

---
+++ crud_productos/registrar_productos.php
@@ -0,0 +1,33 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "login";
+
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+if ($conn->connect_error) {
+    die("No hay conexión: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nombre']) && isset($_POST['descripcion']) && isset($_POST['precio']) && isset($_POST['stock'])) {
+    
+    // Obtener los datos del formulario
+    
+    $nombre = $_POST['nombre'];
+    $descripcion = $_POST['descripcion'];
+    $precio = $_POST['precio'];
+    $stock = $_POST['stock'];
+
+    // Preparar la consulta para insertar un nuevo producto
+    $sql = "INSERT INTO productos (nombre, descripcion, precio, stock) VALUES ('$nombre', '$descripcion', '$precio', '$stock')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Producto agregado correctamente";
+    } else {
+        echo "Error al agregar el producto: " . mysqli_error($conn);
+    }
+}
+
+?>
