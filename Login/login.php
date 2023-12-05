6c6ac73c886826f6bc77c1b51ac160e7e91c7fd5
Pedro-Cuevas-dev <118694793+Pedro-Cuevas-dev@users.noreply.github.com>
2023-12-05 10:48:35 -0600

    Update login.php

--- Login/login.php
+++ Login/login.php
@@ -1,34 +1,73 @@
 <?php
-session_start(); // Iniciar la sesión
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Configurar la conexión a la base de datos
+    $host = "localhost";
+    $usuario_db = "root";
+    $contrasena_db = "";
+    $nombre_db = "login";
 
-$servidor = "localhost"; // Definir el nombre del servidor de la base de datos
-$usuario = "root"; // Definir el nombre de usuario para la base de datos
-$contra = ""; // Definir la contraseña para la base de datos
-$basedatos = "login"; // Definir el nombre de la base de datos
+    // Crear la conexión
+    $conexion = new mysqli($host, $usuario_db, $contrasena_db, $nombre_db);
 
-// Crear conexión
-$conn = new mysqli($servidor, $usuario, $contra, $basedatos); // Establecer una conexión con la base de datos
+    // Verificar si la conexión fue exitosa
+    if ($conexion->connect_error) {
+        die("Error de conexión: " . $conexion->connect_error);
+    }
 
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("mensaje". $conn->connect_error); // Mostrar un mensaje de error si la conexión falla
+    // Obtener datos del formulario
+    $accion = $_POST["accion"];
+    $usuario = $_POST["usuario"];
+    $correo = $_POST["correo"];
+    $contrasena = $_POST["contrasena"];
+
+    // Verificar si es un intento de inicio de sesión o de registro
+    if ($accion === "login") {
+        // Consulta preparada para verificar el inicio de sesión
+        $consulta = $conexion->prepare("SELECT * FROM usuario WHERE usuario = ? AND correo = ? AND contrasena = ?");
+        $consulta->bind_param("sss", $usuario, $correo, $contrasena);
+        $consulta->execute();
+
+        // Obtener resultados
+        $resultado = $consulta->get_result();
+
+        // Verificar si se encontró un usuario con las credenciales proporcionadas
+        if ($resultado->num_rows > 0) {
+            // Inicio de sesión exitoso
+            // Redirigir a la página de producto.php
+            header("Location: ../producto.php");
+            exit();
+        } else {
+            // Credenciales incorrectas
+            echo "Usuario o contraseña incorrectos. Inténtalo de nuevo.";
+        }
+    } elseif ($accion === "registro") {
+        // Consulta preparada para verificar si el correo ya existe
+        $consulta_correo_existente = $conexion->prepare("SELECT * FROM usuario WHERE correo = ?");
+        $consulta_correo_existente->bind_param("s", $correo);
+        $consulta_correo_existente->execute();
+
+        // Obtener resultados
+        $resultado_correo_existente = $consulta_correo_existente->get_result();
+
+        // Verificar si el correo ya existe
+        if ($resultado_correo_existente->num_rows > 0) {
+            // El correo ya está registrado
+            echo "El correo ya está registrado. Inténtalo con otro correo.";
+        } else {
+            // El correo no existe, proceder con el registro
+            // Consulta preparada para insertar nuevo usuario
+            $consulta_registro = $conexion->prepare("INSERT INTO usuario (usuario, correo, contrasena) VALUES (?, ?, ?)");
+            $consulta_registro->bind_param("sss", $usuario, $correo, $contrasena);
+            $consulta_registro->execute();
+
+            // Registro exitoso
+            echo "Registro exitoso. ¡Bienvenido!";
+        }
+    }
+
+    // Cerrar la conexión
+    // $consulta->close();
+    $conexion->close();
 }
-
-$username = $_POST['username']; // Obtener el nombre de usuario del formulario
-$password = $_POST['password']; // Obtener la contraseña del formulario
-
-// Consulta SQL para buscar el nombre de usuario y la contraseña en la base de datos
-$sql = "SELECT username, password FROM usuario WHERE username = '$username' AND password = '$password'";
-
-$result = $conn->query($sql); // Ejecutar la consulta SQL y almacenar el resultado
-
-// Verificar si la consulta devuelve algún resultado
-if ($result->num_rows > 0) {
-    $_SESSION["username"]= $username; // Iniciar sesión y almacenar el nombre de usuario en la variable de sesión
-    header("location: ../index.php"); // Redirigir al usuario a la página de inicio si la sesión es exitosa
-} else {
-    echo "Inicio de sesion fallida. <a href = '../login.php'> Intentar de nuevo </a>"; // Mostrar un mensaje de error si el inicio de sesión falla y proporcionar un enlace para intentar iniciar sesión nuevamente
-}
-
-$conn->close(); // Cerrar la conexión con la base de datos
 ?>
