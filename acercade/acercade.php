2660e969485f3a078b60deb941f61d6470a6027f
antel18 <147548199+antel18@users.noreply.github.com>
2023-12-05 12:46:31 -0600

    Create acercade.php

---
+++ acercade/acercade.php
@@ -0,0 +1,93 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/acerca.css">
+    <link rel="stylesheet" href="CSS/navbar.css">
+    <link rel="stylesheet" href="CSS/footer.css">
+
+     <!--box icons link-->
+     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+     <!--google fonts link-->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Michroma&family=MuseoModerno:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+         <!-- Iconos del navbar -->
+     <head>
+         <!-- Estilos de Font Awesome, carrito de compras -->
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+         <!--  Estilos de Font Awesome, icono usuario  -->
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+     </head>
+    <title>Test doc</title>
+</head>
+<body>
+
+   <!--Header-->
+
+   <header>
+    <a href="index.php" class="logo"><i class='bx bxs-diamond'></i> Hopper</a>
+
+    <ul class="navlist">
+        <!-- <li><a href="index.html" class="active">Inicio</a></li> -->
+        <li><a href="acercade.php">Acerca de nosotros</a></li>
+        <li><a href="pantalla_cop.php">Tienda</a></li>
+        <!-- <li><a href="regist.html">Registrate</a></li> -->
+        <!-- <li><a href="Log.html">Iniciar Sesión</a></li> -->
+        <!-- Iconos del navbar -->
+        <li><a href="carrito_cop.php"><i class="fas fa-shopping-cart"></i></a></li>  
+        <li><a href="Login.php"><i class="fas fa-user"></i> </a></li>
+    </ul>
+
+    <div class="h-main">
+        <a href="producto_cop.php" class="h-btn">Cotizar</a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+        
+    </div>
+</header>
+<!--header end-->
+    <div class="middleside">
+        <h1>Acerca de Nosotros</h1>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non consectetur lectus, quis maximus dui. 
+            Nullam vel est sagittis, mattis ante a, faucibus orci. Orci varius natoque penatibus et magnis dis parturient montes, 
+            nascetur ridiculus mus. Nullam tristique elit sit amet enim vehicula, et ultrices lacus efficitur. 
+            Quisque commodo ipsum felis, sit amet porta massa bibendum id. 
+            <br>
+            Etiam tincidunt, ligula at vulputate efficitur, turpis massa consequat dolor, in commodo nulla est a justo. 
+            Maecenas vel pharetra lectus. Suspendisse fermentum posuere ligula quis lobortis. Curabitur ut aliquam enim. 
+            Integer vulputate diam ut odio placerat, ac porttitor velit dictum. In posuere tellus vel auctor convallis. 
+            Sed id mauris sit amet mi ultrices tristique nec quis erat.
+            <br>
+            
+             
+    </div>
+    <!-- footer -->
+    <footer>
+      <div class="waves">
+          <div class="wave" id="wave1"></div>
+          <div class="wave" id="wave2"></div>
+          <div class="wave" id="wave3"></div>
+          <div class="wave" id="wave4"></div>
+      </div>
+      <ul class="social_icon">
+          <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+          <li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
+          <li><a href="#"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+          <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+      </ul>
+      <ul class="menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Team</a></li>
+          <li><a href="#">Contact</a></li>
+      </ul>
+      <p>&copy Hopper team | Todos los derechos reservados</p>
+  </footer>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
