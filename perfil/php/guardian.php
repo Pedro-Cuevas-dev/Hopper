c3358453546e5a87b3eb6b787a529fb2ef8809a0
antel18 <147548199+antel18@users.noreply.github.com>
2023-12-05 09:24:37 -0600

    Delete perfil directory

--- perfil/php/guardian.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if(!isset($_SESSION['password'])){ //Checa si hay un usuario antes de entrar a la pagina de welcome
-    header("Location ../index.html"); 
-    session_destroy();
-}
-
-?>
